8d255b9a617d60d56d589ff708f690781a724974
KuzkaAndrii <andriykuzka@knu.com>
2024-01-25 21:18:58 +0200

    r point

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def vivaMechMat():
+    print("Viva mech-mat faculty!!!")
 def helloworld():
     print("Hello, world")
 if name=="__main__":
     for i in range(10):
         helloworld()
+    vivaMechMat()
